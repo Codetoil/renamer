7f5b61e537074cb58ae763d778f421e9add9aca0
Angelina Michalek <ianthisawesomee@gmail.com>
2026-04-06 03:47:23 -0400

    Add geckolib for example with mixin

--- renamer-gradle-demo/src/main/java/net/minecraftforge/renamer/gradle/demo/TestClass.java
+++ renamer-gradle-demo/src/main/java/net/minecraftforge/renamer/gradle/demo/TestClass.java
@@ -4,12 +4,14 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
+import mezz.jei.api.JeiPlugin;
 import net.minecraftforge.srgutils.Mappings;
 
 public class TestClass {
     public static void main(String[] args) throws IOException {
         System.out.println(TestClass.class.getName());
         Mappings.load(new ByteArrayInputStream("foo bar".getBytes(StandardCharsets.UTF_8)));
+        System.out.println(JeiPlugin.class.getName());
     }
 
     private static void func_1234_() {
